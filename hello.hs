42b36b8d38be3d026471f9604a0563bd3db95c8a
Rafał Pawełek <rpawelek@proton.me>
2024-02-07 13:59:49 +0100

    Add type signatures and abstract element creation code

--- hello.hs
+++ hello.hs
@@ -1,10 +1,33 @@
+main :: IO ()
 main = putStrLn myhtml
 
-myhtml = makeHtml "Hello title" "Hello, world!"
+myhtml :: String
+myhtml =
+  makeHtml
+    "Hello title"
+    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
